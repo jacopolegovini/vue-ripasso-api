7e8cb7916fffb5558aed3f64fb2dae4a9da10b5f
jacopolegovini <legovini96@gmail.com>
2024-11-20 17:44:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      api: 'http://127.0.0.1:8000/api/posts'
+    }
+  },
+  methods: {
+    getApi(api) {
+      axios.get(api)
+        .then(function (response) {
+          // handle success
+          console.log(response.data.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  },
+  mounted() {
+    this.getApi(this.api)
+  }
+}
+
+
+</script>
+
+<template>
+  <header>
+    <h1>Social API</h1>
+  </header>
+
+  <main>
+    <section>Vari dati</section>
+  </main>
+</template>
+
+<style scoped></style>
